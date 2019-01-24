187bde8ed8a928e1b70b8276d7f63c59debd7f88
MUBUANGA YATE <arnoldmubs@gmail.com>
2019-01-24 11:28:24 +0100

    traitement d'un document avec XML : affichage du nom, nombre d'élément enfant et le nom

--- xpath.js
+++ xpath.js
@@ -1,13 +1,22 @@
-const input_xml = documentquerySelector("#input_xml");
-const input_xpath = documentquerySelector("#input_xpath");
+const input_xml = document.querySelector("#input_xml");
+const input_xpath = document.querySelector("#input_xpath");
 
 //outputs
 
-const out_root = documentquerySelector("#out_root");
-const out_count = documentquerySelector("#out_count");
-const out_children = documentquerySelector("#out_children");
+const out_root = document.querySelector("#out_root");
+const out_count = document.querySelector("#out_count");
+const out_children = document.querySelector("#out_children");
+
+//parser
+
+const parser = new DOMParser();
+let xmldoc;
 
 input_xml.onkeyup = function(){
+    xmldoc = parser.parseFromString(this.value, 'text/xml');
+    out_root.innerText = xmldoc.documentElement.tagName;
+    out_count.innerText = xmldoc.documentElement.childElementCount;
+    out_children.innerText = xmldoc.documentElement.children[0].tagName;
     console.log(this.value);
 };
 

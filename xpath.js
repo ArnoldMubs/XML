45f655c8e4e2d29a88ef4c37da30249ed6ac3caf
MUBUANGA YATE <arnoldmubs@gmail.com>
2019-01-24 12:14:07 +0100

    evaluate with xpath

--- xpath.js
+++ xpath.js
@@ -6,20 +6,40 @@ const input_xpath = document.querySelector("#input_xpath");
 const out_root = document.querySelector("#out_root");
 const out_count = document.querySelector("#out_count");
 const out_children = document.querySelector("#out_children");
+const out_xpath = document.querySelector("#out_xpath");
+
 
 //parser
 
 const parser = new DOMParser();
 let xmldoc;
+let nodes;
+let result;
 
 input_xml.onkeyup = function(){
     xmldoc = parser.parseFromString(this.value, 'text/xml');
     out_root.innerText = xmldoc.documentElement.tagName;
     out_count.innerText = xmldoc.documentElement.childElementCount;
     out_children.innerText = xmldoc.documentElement.children[0].tagName;
-    console.log(this.value);
+    //console.log(this.value);
 };
 
+let str;
 input_xpath.onkeyup = function(){
-    console.log(this.value);
+    let concat = ""
+    nodes = xmldoc.evaluate(this.value, xmldoc, null,
+        XPathResult.ANY_TYPE, null);
+    try {
+        result = nodes.iterateNext();
+        while (result) {
+            str = new XMLSerializer().serializeToString(result);
+            concat += str;
+            result = nodes.iterateNext();
+        }
+        out_xpath.innerText = concat;
+    }catch (e) {
+        out_xpath.innerText = 'Erreur : L\'arbre du document a été modifié pendant l\'itération ' + e ;
+    }
+
+
 };
